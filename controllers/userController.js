837f96e3e304004a1b553dc2a90c013025962484
Akshansh Shrivastava <Akshansh773@gmail.com>
2022-03-03 00:12:45 +0530

    added updating user info

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,17 @@ const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/AppError');
 
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach(el => {
+    if (allowedFields.includes(el)) {
+      newObj[el] = obj[el];
+    }
+  });
+
+  return newObj;
+};
+
 exports.getAllUsers = catchAsync(async (req, res) => {
   const users = await User.find();
 
@@ -10,6 +21,34 @@ exports.getAllUsers = catchAsync(async (req, res) => {
     results: users.length,
     data: {
       users
+    }
+  });
+});
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1.  if user tried to update password
+  if (req.body.password || req.body.confirmPassword) {
+    return next(
+      new AppError(
+        'This route is not for password updates, please use /updateMyPassword',
+        400
+      )
+    );
+  }
+
+  // body.role = admin security breach
+
+  const filteredBody = filterObj(req.body, 'name', 'email');
+  
+  const updatedUser = await User.findByIdAndUpdate(req.user._id, filteredBody, {
+    new: true,
+    runValidators: true
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser
     }
   });
 });
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -15,6 +15,14 @@ router.patch(
   authController.updatePassword
 );
 
+router.patch(
+  '/updateMe',
+  authController.protect,
+  userController.updateMe
+);
+
+
+
 router
   .route('/')
   .get(userController.getAllUsers)
