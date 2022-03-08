023452b2f6f27b7a5b8b696de5446d74e3a3ea71
Akshansh Shrivastava <Akshansh773@gmail.com>
2022-03-08 16:54:22 +0530

    user and password update done

--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -1,27 +1,57 @@
 import { showAlert } from './alerts.js';
-const updateUserData = async () => {
-  const name = document.querySelector('.form-user-data #name').value;
-  const email = document.querySelector('.form-user-data #email').value;
 
+/// type is either passsword or data
+const updateSettings = async (data, type) => {
   try {
+    const url =
+      type === 'password'
+        ? 'http://localhost:3000/api/v1/users/updateMyPassword'
+        : 'http://localhost:3000/api/v1/users/updateMe';
+
     const res = await axios({
       method: 'PATCH',
-      url: 'http://localhost:3000/api/v1/users/updateMe',
-      data: {
-        name,
-        email
-      }
+      url,
+      data
     });
 
     if (res.data.status === 'success') {
-      showAlert('success', 'User info updated');
+      showAlert('success', `${type.toUpperCase()} Updated successfully `);
     }
   } catch (error) {
-    showAlert('error', 'something went wrong');
+    showAlert('error', error.response.data.message);
   }
 };
 
 document.querySelector('.form-user-data').addEventListener('submit', e => {
   e.preventDefault();
-  updateUserData();
+  const name = document.querySelector('.form-user-data #name').value;
+  const email = document.querySelector('.form-user-data #email').value;
+  updateSettings({ name, email }, 'data');
 });
+
+document
+  .querySelector('.form-user-password')
+  .addEventListener('submit', async e => {
+    e.preventDefault();
+
+    // <div class="spinner-border text-success"></div>
+    document.querySelector(
+      '.btn--save-password'
+    ).textContent = "Updating ....";
+
+    const passwordCurrent = document.getElementById('password-current').value;
+    const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('password-confirm').value;
+    // awaitng it so that we can wait for it complete , and we then clear off the fields
+    await updateSettings(
+      { passwordCurrent, password, confirmPassword },
+      'password'
+    );
+
+    document.getElementById('password-current').value = '';
+
+    document.getElementById('password').value = '';
+
+    document.getElementById('password-confirm').value = '';
+    document.querySelector('.btn--save-password').textContent = 'Save Password';
+  });
