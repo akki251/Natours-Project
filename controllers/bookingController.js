6caa4fb110349079e2b764e5e7d0be995af25ef0
Akshansh Shrivastava <Akshansh773@gmail.com>
2022-03-11 19:08:35 +0530

    Stripe url changed

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -15,7 +15,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // 2// create the checkout session
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    success_url: `https://natours-tour-project.herokuapp.com/?tour=${
+    success_url: `${req.protocol}://${req.get('host')}/my-tours/?tour=${
       req.params.tourID
     }&user=${req.user.id}&price=${tour.price}`,
     cancel_url: `${req.protocol}://localhost:3000/tour/${tour.slug}`,
