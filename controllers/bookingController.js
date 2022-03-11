235ac02d097b82dbc6977c4eba839495228a3bd8
Akshansh Shrivastava <Akshansh773@gmail.com>
2022-03-11 18:33:10 +0530

    stripe url fix

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -2,7 +2,7 @@ const stripe = require('stripe')(
   'sk_test_51KblnYSHXreynmNIb9gaRhvrfRHok9WdFvgxCLvG7Nf63LiJ80z6s3jh7lZYE4KrDZrmrDWgcmY8f4mW2zQNY1YS002UbeuOJ1'
 );
 const catchAsync = require('../utils/catchAsync');
-const AppError = require('../utils/appError');
+const AppError = require('../utils/AppError');
 const Tour = require('../models/tourModel');
 const Factory = require('./handlerFactory');
 const Booking = require('../models/bookingModel');
@@ -15,7 +15,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // 2// create the checkout session
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    success_url: `${req.protocol}://localhost:3000/?tour=${
+    success_url: `${req.protocol}://${req.get('host')}/?tour=${
       req.params.tourID
     }&user=${req.user.id}&price=${tour.price}`,
     cancel_url: `${req.protocol}://localhost:3000/tour/${tour.slug}`,
